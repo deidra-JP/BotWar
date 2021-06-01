7a73d81cd68c9405b1d88a9d4caaa918de5db58e
deidra-JP <deidra@outlook.jp>
2021-06-01 22:20:02 +0900

    0_0_1

---
+++ contracts/bot/BotOwnership.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../token/ERC721/ERC721.sol";
+import "../utils/math/Safemath.sol";
+import "../access/Ownable.sol";
+
+contract BotOwnership is ERC721 {
+
+}
+    
